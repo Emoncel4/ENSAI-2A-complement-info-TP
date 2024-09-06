80638dbaa59b657b8dacf8458974cf80ada09bf3
Emoncel4 <parallelcord@gmail.com>
2024-09-06 11:45:03 +0200

    Modifications cool

--- src/Emile.py
+++ src/Emile.py
@@ -3,8 +3,4 @@ a, b = 0, 5
 for i in range(b):
     print(" " * a, end="")
     a += 1
-<<<<<<< HEAD
     print("Bonjour Emile2222")
-=======
-    print("Bonjour Emile-Leo")
->>>>>>> c7b268f65094324488dd2a8decdbf7e7b200af1c
