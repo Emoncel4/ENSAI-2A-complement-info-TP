72039b03da6887e3737bd03908f86e7b8dc7700c
Yatoute MINTOAMA <yatoute.mintoama@eleve.ensai.fr>
2024-09-06 11:52:14 +0200

    correction erreur

--- src/Emile.py
+++ src/Emile.py
@@ -3,12 +3,7 @@ a, b = 0, 5
 for i in range(b):
     print(" " * a, end="")
     a += 1
-    print("Bonjour Emile2222")
-<<<<<<< HEAD
-=======
     print("Bonjour Emile-Leo")
 
 for i in range(b):
     print(i, "*", b, " = ", i * b)
->>>>>>> c7b268f65094324488dd2a8decdbf7e7b200af1c
-=======
